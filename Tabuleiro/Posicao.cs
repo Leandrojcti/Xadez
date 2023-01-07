ad60dd04c0098ffc4a5b6c2e7072d3309c1574af
Leandro Jose <leandrojose2226@gmail.com>
2023-01-07 13:56:44 -0300

    Inicio da implementação

---
+++ Tabuleiro/Posicao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro {
+    class Posicao {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao(int linha, int coluna) {
+            Linha = linha;
+            Coluna = coluna;
+        }
+    }
+}
---
+++ Tabuleiro/Tabuleiro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro {
+    class Tabuleiro {
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+
+        private Peca[,] pecas;  //Declarado uma matriz do tipo Peca
+
+        public Tabuleiro(int linhas, int colunas) {
+            Linhas = linhas;
+            Colunas = colunas;
+            pecas = new Peca[linhas, colunas]; //ja instancia e seta linhas e colunas com argumento
+        }
+    }
+}
