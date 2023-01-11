63988c692f3e9b25ab7179f650edbfe2df16b822
Leandro Jose <leandrojose2226@gmail.com>
2023-01-10 22:04:03 -0300

    Melhorando a classe tela

--- Program.cs
+++ Program.cs
@@ -8,8 +8,8 @@ namespace Xadrez {
             try {
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0,2));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(0,2));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(1, 3));
                 
                 tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 5));
 
--- Tela.cs
+++ Tela.cs
@@ -7,20 +7,33 @@ namespace Xadrez {
     class Tela {
         public static void imprimirTabuleiro(Tabuleiro tab) {
             for (int i_L = 0; i_L < tab.Linhas; i_L++) {
+                Console.Write(8 - i_L + " ");
                 for (int j_C = 0; j_C < tab.Colunas; j_C++) {
                     if (tab.peca(i_L, j_C) ==null) {
                         Console.Write("- ");
                     } else {
-                        Console.Write(tab.peca(i_L, j_C) + "");
+                        ImprimirPeça(tab.peca(i_L, j_C));
                     }
                 }
                 Console.WriteLine();
-
-
-
-
-
+            }
+            Console.WriteLine( "  a b c d e f g h");
+        }
+        public static void ImprimirPeça(Peca peca) {
+            if (peca.Cor == Cor.Branca) {
+                Console.Write(peca);
+            } else {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
+
     }
 }
+
+
+
+
+
